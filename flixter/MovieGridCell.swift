a07467a10c17e4c2aeeaa0a4b9d9c485865f58f5
digitaluniverse <contact@mattdaniele.com>
2020-02-06 20:08:24 -0800

    first flix part 2 finished commit

---
+++ flixter/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixter
+//
+//  Created by Matthew Daniele on 2/6/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
